3aec4df66219738a7f64283e098869b56bc5b005
John Chadwick <john@jchw.dev>
2026-08-23 00:39:26 -0400

    Fix broken tests, curl-adapter

--- overlay/packages/curl-adapter/package.nix
+++ overlay/packages/curl-adapter/package.nix
@@ -3,6 +3,7 @@
   buildPythonPackage,
   fetchPypi,
   setuptools,
+  brotli,
   requests,
   curl-cffi,
   pycurl,
@@ -10,29 +11,18 @@
 
 buildPythonPackage rec {
   pname = "curl_adapter";
-  version = "1.0.0.post2";
+  version = "1.2.2";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-sVpje78NHamLOCDfj4B/e6wPT5HWh0ARA4dr8Vn6geM=";
+    hash = "sha256-TnZUa18Djgbfk/nx+OPCqWqziKQaqVnrNJ7U/dcSgMg=";
   };
 
   build-system = [ setuptools ];
 
-  postPatch = ''
-    substituteInPlace pyproject.toml \
-      --replace-fail 'pycurl==7.45.3' 'pycurl>=7.45.3'
-
-    # Fix compatibility with urllib3 >= 2.6 which passes max_length to _decode()
-    substituteInPlace curl_adapter/stream/response.py \
-      --replace-fail 'def _decode(self, data, decode_content, flush_decoder):' \
-                     'def _decode(self, data, decode_content, flush_decoder, max_length=None):' \
-      --replace-fail 'return super()._decode(data, decode_content, flush_decoder)' \
-                     'return super()._decode(data, decode_content, flush_decoder, max_length=max_length)'
-  '';
-
   dependencies = [
+    brotli
     requests
     curl-cffi
     pycurl
